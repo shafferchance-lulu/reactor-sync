17243ff46906a297fa3a9b4b5c2264b8fd5d3d0f
Chanceler <cshaffer2@lululemon.com>
2024-06-07 15:20:17 -0400

    Adding slight delay due to getting 429'd

--- bin/utils/paginatedRequest.js
+++ bin/utils/paginatedRequest.js
@@ -1,22 +1,25 @@
-export async function* requestPageGenerator(request, pageStart, pageFinish, ...args) {
+async function* requestPageGenerator(request, pageStart, pageFinish, ...args) {
     let currentPage = pageStart + 1;
     while (currentPage <= pageFinish) {
+        await new Promise((res) => setTimeout(res), Math.random() * 10)
         yield request(...args);
-      currentPage++;
+        currentPage++;
     }
 }
 
-export async function getAllDataForRequest(sdkFunction, ...args) {
-    const rawData = await sdkFunction(...args);
-    
-    if (rawData.meta?.pagination) {
-        const { current_page: currentPage, total_pages: totalPages } = rawData.meta.pagination;
-        let data = [...rawData.data];
-        for await (const request of requestPageGenerator(sdkFunction, currentPage, totalPages, ...args)) {
-            data.push(...request.data);
+module.exports = {
+    getAllDataForRequest: async function (sdkFunction, ...args) {
+        const rawData = await sdkFunction(...args);
+
+        if (rawData.meta?.pagination) {
+            const { current_page: currentPage, total_pages: totalPages } = rawData.meta.pagination;
+            let data = [...rawData.data];
+            for await (const request of requestPageGenerator(sdkFunction, currentPage, totalPages, ...args)) {
+                data.push(...request.data);
+            }
+            return data;
+        } else {
+            return rawData.data;
         }
-        return data;
-    } else {
-        return rawData.data;
     }
 }
